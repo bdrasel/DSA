28970c195759de209a9827bb1c68262707647a14
bdrasel <rh88248@gmail.com>
2025-05-17 15:33:46 +0600

    analyze time & space complexity

--- space_complexity.js
+++ space_complexity.js
@@ -1,12 +1,12 @@
-function sum ( arr) {
-    let total = 0;
-    for (let i =0; i < arr.length; i++) {
-        total += arr[i];
-    }   
-    return total;
-}
+// function sum ( arr) {
+//     let total = 0;
+//     for (let i =0; i < arr.length; i++) {
+//         total += arr[i];
+//     }   
+//     return total;
+// }
 
-console.log(sum([20,50,63,96,100,50,83]));
+// console.log(sum([20,50,63,96,100,50,83]));
 
 
 // function double (arr) {
@@ -17,3 +17,23 @@ console.log(sum([20,50,63,96,100,50,83]));
 //     return newArr;
 // }
 // console.log(double([2,5,5,8,10,60]));
+
+
+function nameIdentify(arr) {
+    let uniqueArr = [];
+    for(let i =0; i < arr.length; i++) { //n
+        let element = arr[i];
+        if(!uniqueArr.includes(element)) {
+            uniqueArr.push(element)
+        }
+    }
+    return uniqueArr; //n
+
+}
+
+//Time Complexity n2
+//Space Complexity n
+
+let arrData = ["Rasel", "Yeasin", "Ismail", "Rasel", "Raju", "Ismail"];
+
+console.log(nameIdentify(arrData));
